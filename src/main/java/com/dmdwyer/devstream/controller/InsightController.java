9fd5502b8e6ef5c2b1db6c5a231e0c653069e221
DMDwyer <dmjdwyer@gmail.com>
2025-11-08 19:12:05 +0000

    fix: Project setup, improved dockerfile and added h2 datasource

--- src/main/java/com/dmdwyer/devstream/controller/InsightController.java
+++ src/main/java/com/dmdwyer/devstream/controller/InsightController.java
@@ -1,9 +1,17 @@
+package com.dmdwyer.devstream.controller;
+
+import com.dmdwyer.devstream.entity.Insight;
+import com.dmdwyer.devstream.service.InsightService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/insights")
 public class InsightController {
     private final InsightService service;
-    public InsightController(InsightService service){
+
+    public InsightController(InsightService service) {
         this.service = service;
     }
 
--- src/main/java/com/dmdwyer/devstream/entity/Insight.java
+++ src/main/java/com/dmdwyer/devstream/entity/Insight.java
@@ -1,10 +1,46 @@
+package com.dmdwyer.devstream.entity;
+
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
 @Entity
 public class Insight {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String topic;
     private String detail;
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
