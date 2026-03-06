3b6c568e492b19a0143db30465b4717ffa0db064
DMDwyer <dmjdwyer@gmail.com>
2026-03-06 01:43:57 +0000

    feat(terraform): add helm provider and helm_release resource

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,11 +6,21 @@ terraform {
             source  = "hashicorp/kubernetes"
             version = "~> 2.26"
         }
+        helm = {
+            source = "hashicorp/helm"
+            version = "~> 2.12"
+        }
     }
 }
 
 provider "kubernetes" {
     config_path = var.kubeconfig_path
+}
+
+provider "helm" {
+    kubernetes {
+        config_path = var.kubeconfig_path
+    }
 }
 
 resource "kubernetes_namespace" "devstream" {
@@ -28,4 +38,17 @@ resource "kubernetes_config_map" "devstream_config" {
     data = {
         FEATURE_FLAGS_DEFAULT = "false"
     }
+}
+
+resource "helm_release" "devstream" {
+    name       = "devstream-portfolio"
+    chart      = "${path.module}/../helm"
+    namespace  = kubernetes_namespace.devstream.metadata[0].name
+
+    set {
+        name  = "image.tag"
+        value = var.image_tag
+    }
+
+    depends_on = [kubernetes_namespace.devstream]
 }
